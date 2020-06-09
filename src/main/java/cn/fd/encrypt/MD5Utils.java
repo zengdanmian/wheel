5ea0f7dd508eb861c483d30342185538158fdd64
zengdanm <zengdanmian@163.com>
2020-06-09 17:38:09 +0800

    MD5不需要依赖jar包

--- src/main/java/cn/fd/encrypt/MD5Utils.java
+++ src/main/java/cn/fd/encrypt/MD5Utils.java
@@ -1,6 +1,9 @@
 package cn.fd.encrypt;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
+
+import java.security.MessageDigest;
 
 /**
  * 密码散列函数
@@ -9,7 +12,9 @@ import org.springframework.util.DigestUtils;
  * 3. 结果是32位16进制数
  */
 public class MD5Utils {
-    private static final String salt = "salt";// 彩虹表只能破解没加盐的MD5
+    private static final String MD5_ALGORITHM_NAME = "MD5";
+    private static final String SALT = "salt";// 彩虹表只能破解没加盐的MD5
+    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
     public static String encode(String src) {
         try {
@@ -32,8 +37,37 @@ public class MD5Utils {
         return null;
     }
 
-    public static void main(String[] args) {
-        String encode = MD5Utils.encode("87ki3nd7287ki3nd7287ki3nd7287ki3nd72");
-        System.out.println(encode);
+    // 不需要依赖 Spring
+    public static String encode(String input, String salt) {
+        if (StringUtils.isBlank(salt)) {
+            salt = SALT;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+        sb.append(salt);
+
+        String result = null;
+        try {
+            byte[] btInput = sb.toString().getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance(MD5_ALGORITHM_NAME);
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+
+            char[] res = new char[md.length * 2];
+            int k = 0;
+
+            for (byte byteItem : md) {
+                res[k++] = HEX_DIGITS[byteItem >>> 4 & 0xf];
+                res[k++] = HEX_DIGITS[byteItem & 0xf];
+            }
+            result = new String(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
