2d96621525f639f1c00f1f9ee8e138a03b842c1f
zengdanm <zengdanmian@163.com>
2020-06-09 10:09:11 +0800

    增加根据网卡获取本机配置的ip，增加了对多个代理的情况的兼容

--- src/main/java/cn/fd/mvc/IpAddress.java
+++ src/main/java/cn/fd/mvc/IpAddress.java
@@ -1,6 +1,8 @@
 package cn.fd.mvc;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class IpAddress {
     public String getIpAddress(HttpServletRequest request) {
@@ -19,6 +21,26 @@ public class IpAddress {
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
+
+            if (ip.equals("127.0.0.1")) {// 根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                if( inet != null ) {
+                    ip = inet.getHostAddress();
+                }
+            }
+
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ip != null && ip.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ip.indexOf(',') > 0) {
+                    ip = ip.substring(0, ip.indexOf(','));
+                }
+            }
         }
         return ip;
     }
